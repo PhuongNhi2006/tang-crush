ab6e5b6d0df12f973715824249c51d9c3466dc30
PhuongNhi2006 <89259960+PhuongNhi2006@users.noreply.github.com>
2021-08-21 18:53:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: ' Nè nè m ơi <3',
-    introDesc: `T hỏi m một câu nhá! 
+    introTitle: ' Nè nè mày ơi <3',
+    introDesc: `Tao hỏi mày một câu nhá! 
     Phải nói thật lòng đấy!`,
     btnIntro: 'Oke ',
-    title: ' T yêu m nhiều lắm ó 😙',
-    desc: 'M cũng yêu t nhiều lắm phải không ? ',
-    btnYes: 'Đúng rồi,t yêu m vl ýyyyy <333',
-    btnNo: 'Không,t ghét m lắm :3',
-    question:'Sao m yêu t nhiều z ?? <3',
-    btnReply: 'Nhấn vô tim nhi để gửi nè <3 ',
-    reply: 'Yêu thì yêu mà không yêu cũng phải yêu <333333 Không yêu lf t chém m đấy :))',
-    mess: 'T biết ngay mà 🥰. Yêu m nhiều nhiều  😘',
-    messDesc: 'Yêu hy nhiều nhiều😘 ',
+    title: ' Tao yêu mày nhiều lắm ó 😙',
+    desc: 'Mày cũng yêu tao nhiều lắm phải không ? ',
+    btnYes: 'Đúng rồi,tao yêu mày vl ýyyyy <333',
+    btnNo: 'Không,tao ghét mày lắm :3',
+    question:'Sao mày yêu tao nhiều zay hỏ ?? <3',
+    btnReply: 'Nhấn vô tim tao để gửi nè <3 ',
+    reply: 'Yêu thì yêu mà không yêu cũng phải yêu <333333 Không yêu là tao chém mày đấy :))',
+    mess: 'Tao biết ngay mà 🥰. Yêu mày nhiều nhiều  😘',
+    messDesc: 'Yêu nhiều nhiều😘 ',
     btnAccept: 'Okiiiii lun nà  <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
