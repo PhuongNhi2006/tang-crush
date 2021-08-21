8735d899e85b30db61c3f536b8e7779f7c469551
PhuongNhi2006 <89259960+PhuongNhi2006@users.noreply.github.com>
2021-08-21 09:50:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 const CONFIG = {
     introTitle: ' Nè nè  cậu ưiii <3',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'á há há',
-    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
+    introDesc: `Tớ hỏi một câu nhá, phải nói thật lòng đấy, không được điêu đâu <3`,
+    btnIntro: 'Oke nò',
+    title: ' Tớ thích cậu lắm ó, thích nhìu nhìu nhìu lắm luon 😙',
+    desc: 'Có phải cậu cũng thích tớ đúng khum, có phải khong nè cậu <3 ',
     btnYes: 'Tui thích cậu lắm ó <33',
     btnNo: 'Không, cậu bị ảo tưởng sức mạnh hã :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu cậu  <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại yêu mỗi cậu thế nhỉ =((  <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333 Mà ai cho hong yêu hỏ =))',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều  😘',
